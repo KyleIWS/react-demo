60272cf3a6bc57c3d1358e1c9c95b3d17acc1a2b
Kyle <kyleiws@uw.edu>
2017-04-26 22:30:46 -0700

    Working, simple TicTacToe

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 "use strict";
+
+// Remove the right-click menu as default behavior for the window.
+// We do this because we don't want that menu to pop while playing.
+window.oncontextmenu = function() {
+    return false;
+}
+
+// Helpful DOM element getter.
+function $(id) {
+    return document.getElementById(id);
+}
+
+// The board we are playing on
+var board = $("board");
+
+// Attach on the tiles.
+for(var col = 0; col < 3; col++) {
+    for(var row = 0; row < 3; row++) {
+        var tile = document.createElement("div");
+        tile.classList.add("tile");
+        tile.left = (col * 100) + "px";
+        tile.top = (row * 100) + "px";
+
+        tile.onmousedown = choose;      
+        
+        board.appendChild(tile);
+
+    }
+}
+
+// Event handle function that changes tiles
+// to the correct shape.
+function choose(event) {
+    if(event.button == 0) {
+        event.target.classList.add("husky");
+        event.target.classList.remove("coug");
+    } else  if(event.button == 2) {
+        event.target.classList.add("coug");
+        event.target.classList.remove("husky");
+    } else if(event.button == 1) {
+        event.target.classList.remove("husky");
+        event.target.classList.remove("coug");
+    }
+}
